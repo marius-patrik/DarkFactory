58449d04f7f18314de9b6616483907d0e2ea1255
pmarius <69584331+marius-patrik@users.noreply.github.com>
2026-09-20 22:18:28 +0200

    feat(docs): export docs impact policy

--- packages/docs/src/index.ts
+++ packages/docs/src/index.ts
@@ -7,4 +7,4 @@ export * from "./config.ts";
 export * from "./content.ts";
 export * from "./readme.ts";
 export * from "./api.ts";
-export * from "./current.ts";
+export * from "./current.ts";\nexport * from "./impact.ts";
