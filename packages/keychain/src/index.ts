db90c926cf46fc2a544b0342a91833ece031cf14
pmarius <69584331+marius-patrik@users.noreply.github.com>
2026-09-21 09:02:31 +0200

    feat(keychain): expose external credential custody

--- packages/keychain/src/index.ts
+++ packages/keychain/src/index.ts
@@ -8,3 +8,5 @@ export * from "./github-app.ts";
 export * from "./vault.ts";
 export * from "./vault-crypto.ts";
 export * from "./redaction.ts";
+export * from "./external-keyring.ts";
+export * from "./external-source.ts";
